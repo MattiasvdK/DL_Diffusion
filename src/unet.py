b2543252a980c5d41d795caa0727232e49d1a048
Mattias van der Kolk <mattiasv.d.kolk@gmail.com>
2024-03-19 21:26:20 +0100

    Added more code to retreive and transform images. Added file with basic U-Net model

---
+++ src/unet.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3),
+            nn.LeakyReLU(inplace=True, negative_slope=0.1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3),
+            nn.LeakyReLU(inplace=True, negative_slope=0.1),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+    
+
+class Downscale(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Downscale, self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv(x)
+        return x
+    
+    
+class Upscale(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Upscale, self).__init__()
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+    
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+    
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        # Encoder
+        self.layer1 = DoubleConv(in_channels, 64)
+        self.down1 = Downscale(64, 128)
+        self.down2 = Downscale(128, 256)
+        self.down3 = Downscale(256, 512)
+        self.down4 = Downscale(512, 1024)
+
+        # Bottleneck
+        self.bottleneck = DoubleConv(1024, 1024)
+        
+        # Decoder
+        self.up1 = Upscale(1024, 512)
+        self.up2 = Upscale(512, 256)
+        self.up3 = Upscale(256, 128)
+        self.up4 = Upscale(128, 64)
+
+        # Output
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.layer1(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # Bottleneck
+        x = self.bottleneck(x5)
+
+        # Decoder
+        x = self.up1(x, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        # Output
+        x = self.out(x)
+        return x
