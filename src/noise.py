610b5188e27a436ba2c83016c1e2738669f35437
Mattias van der Kolk <mattiasv.d.kolk@gmail.com>
2024-03-26 12:14:02 +0100

    Finished the noise scheduler for batches of images. Needs checking if it is working properly with different devices

--- src/noise.py
+++ src/noise.py
@@ -34,12 +34,13 @@ class CosineScheduler():
     """
 
     def __init__(self, timesteps, s=0.008, device=None):
+        self.device = device
         self.timesteps = timesteps
         time = torch.linspace(0, timesteps, timesteps + 1)
-        alphas = alphas = torch.cos((time / timesteps + s) / (1 + s) * (np.pi / 2))**2
+        alphas = alphas = torch.cos((time / timesteps + s) / (1 + s) * np.pi / 2)**2
         alphas = alphas / alphas[0]
         alphas = alphas[1:] / alphas[:-1]
-        self.betas = torch.clip(1 - alphas, 0, 0.999)
+        self.betas = torch.clip(1 - alphas, 0.0, 0.9999)
 
         self.alphas = 1 - self.betas
         self.alphas_cumprod = torch.cumprod(self.alphas, 0)
@@ -51,20 +52,28 @@ class CosineScheduler():
         
         self.posterior_variance = self.betas * (1 - alphas_cumprod_prev) / (1 - self.alphas_cumprod)
     
-    def __call__(self, img):
+    def __call__(self, img, timestep):
         """Noise image based on schedule."""
-        return self.sample_noise(img)
+        return self.apply_noise(img, timestep)
     
-    def sample_noise(self, img):
-        """Sample noise from schedule."""
-        pass
+    def apply_noise(self, img, timestep):
+        """Apply noise to image."""
+        noise = self._gaussian_noise(img.shape)
+        sqrt_alpha = self._obtain(self.alphas_cumprod_sqrt, timestep, img.shape)
+        sqrt_one_minus_alpha = self._obtain(self.alphas_cumprod_min_sqrt, timestep, img.shape)
+        return sqrt_alpha * img + sqrt_one_minus_alpha * noise
 
     
-    def _obtain(self, timestep, source, target_shape):
+    def _obtain(self, source, timestep, target_shape):
         """Obtain values from target in timestep index for batches.
         Based on the extract function from: https://huggingface.co/blog/annotated-diffusion.
         """
         batch_size = timestep.shape[0]
         values = source.gather(-1, timestep.cpu())
         return values.reshape(batch_size, *((1,) * (len(target_shape) - 1))).to(timestep.device)
+    
+    def _gaussian_noise(self, shape):
+        """Gaussian noise for sampling."""
+        return torch.randn(shape)
+    
     
