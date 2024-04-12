c7ad713b73b5576dc305d6cd7ce189e259665a9d
rizkiduwinanto <rizkiduwinanto@gmail.com>
2024-04-12 21:55:39 +0200

    edit jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -15,11 +15,11 @@ unzip /scratch/$USER/test2014.zip -d $TMPDIR/dataset
 unzip /scratch/$USER/val2014.zip -d $TMPDIR/dataset
 
 # Copy to TPMDIR
-cp /home4/$USER/DL_Diffusion $TMPDIR
-cd $TMPDIR
+cp -R /home4/$USER/DL_Diffusion $TMPDIR/DL_Diffusion
+cd $TMPDIR/DL_Diffusion
 
 # Run the training
-/home4/$USER/venvs/rizkienvs/bin/python3 src/main.py
+/home4/$USER/venvs/rizkienvs/bin/python3 $TMPDIR/DL_Diffusion/src/main.py
 
-mkdir -p /home4/$USER/QA_jobs/job_${SLURM_JOBID}
-tar czvf /home4/$USER/QA_jobs/job_${SLURM_JOBID}/results.tar.gz $TMPDIR/results
+mkdir -p /home4/$USER/job_${SLURM_JOBID}
+tar czvf /home4/$USER/job_${SLURM_JOBID}/results.tar.gz $TMPDIR/results
