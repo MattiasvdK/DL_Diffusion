fb2afc6a6079e024c17e9599fd1ba4e47bed2e95
rizkiduwinanto <rizkiduwinanto@gmail.com>
2024-04-14 15:02:22 +0200

    reduce batch size

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def main():
         "/tmp/dataset/train2014/",
         "/tmp/dataset/val2014/",
         "/tmp/dataset/test2014/",
-        batch_size=64,
+        batch_size=32,
         )
     train_diffusion(
         model=model,
