7250e1dc6096f1e2a71c66c2df9b0683e5043a0a
rizkiduwinanto <rizkiduwinanto@gmail.com>
2024-04-14 14:37:31 +0200

    add data loader and main

--- src/main.py
+++ src/main.py
@@ -17,11 +17,11 @@ def main():
         "/root/DL_Diffusion/dataset/test2014",
         "/root/DL_Diffusion/dataset/val2014/",
         "/root/DL_Diffusion/dataset/test2014/",
-        batch_size=16,
+        batch_size=64,
         )
     train_diffusion(
         model=model,
-        scheduler=CosineScheduler(timesteps=500),
+        scheduler=CosineScheduler(timesteps=1000),
         train_loader=train_loader,
         val_loader=val_loader,
         test_loader=test_loader,
