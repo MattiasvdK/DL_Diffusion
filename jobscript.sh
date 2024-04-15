e3aa79f2fa32eb911632682550d0b39b6b35cfe6
rizkiduwinanto <rizkiduwinanto@gmail.com>
2024-04-15 12:55:20 +0200

    add jobs

--- jobscript.sh
+++ jobscript.sh
@@ -5,6 +5,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=12GB
 #SBATCH --partition=gpu
+#SBATCH --signal=B:12@600
 
 mkdir $TMPDIR/dataset
 mkdir -p $TMPDIR/results
@@ -14,12 +15,16 @@ unzip /scratch/$USER/train2014.zip -d $TMPDIR/dataset
 unzip /scratch/$USER/test2014.zip -d $TMPDIR/dataset
 unzip /scratch/$USER/val2014.zip -d $TMPDIR/dataset
 
+# Compress and save the results if the timelimit is close
+trap 'mkdir -p /home4/$USER/job_${SLURM_JOBID}; tar czvf /home4/$USER/job_${SLURM_JOBID}/results.tar.gz $TMPDIR/results' 12
+
 # Copy to TPMDIR
 cp -R /home4/$USER/DL_Diffusion $TMPDIR/DL_Diffusion
 cd $TMPDIR/DL_Diffusion
 
 # Run the training
-/home4/$USER/venvs/rizkienvs/bin/python3 $TMPDIR/DL_Diffusion/src/main.py
+/home4/$USER/venvs/rizkienvs/bin/python3 $TMPDIR/DL_Diffusion/src/main.py &
+wait
 
 mkdir -p /home4/$USER/job_${SLURM_JOBID}
 tar czvf /home4/$USER/job_${SLURM_JOBID}/results.tar.gz $TMPDIR/results
