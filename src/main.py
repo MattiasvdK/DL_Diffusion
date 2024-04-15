22287100f57ded9f155440ad416868fd7c018d2c
rizkiduwinanto <rizkiduwinanto@gmail.com>
2024-04-15 15:06:11 +0200

    add batch size

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def main():
         "/root/DL_Diffusion/dataset/test2014",
         "/root/DL_Diffusion/dataset/val2014/",
         "/root/DL_Diffusion/dataset/test2014/",
-        batch_size=256,
+        batch_size=128,
         )
     train_diffusion(
         model=model,
